126bfde65a6f713da306feef8517f0e52dcdd3fe
tilltnet <tilltnet@users.noreply.github.com>
2014-11-28 22:47:36 +0100

    Update __init__.py

--- raumfeld/__init__.py
+++ raumfeld/__init__.py
@@ -98,6 +98,18 @@ class RaumfeldDevice(object):
         """Start playing"""
         self.av_transport.Play(InstanceID=1, Speed=2)
 
+
+    def play(self, value):
+        self.av_transport.SetAVTransportURI(InstanceID=1, CurrentURI=value, CurrentURIMetaData="")
+
+    def next(self):
+        """Next"""
+        self.av_transport.Next(InstanceID=1)
+
+    def previous(self):
+        """Previous"""
+        self.av_transport.Previous(InstanceID=1)
+
     def pause(self):
         """Pause"""
         self.av_transport.Pause(InstanceID=1)
